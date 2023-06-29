40fe8b53511ebc40e5ddaa56422dcbfb95f5b50b
Ihor Rohovets <freego555@gmail.com>
2023-06-29 14:28:12 +0300

    Add basic api methods to messenger

--- JavaScript/y1-portfolio/api/messenger.js
+++ JavaScript/y1-portfolio/api/messenger.js
@@ -1,6 +1,35 @@
 ({
-  async method({ arg }) {
-    console.log({ method: 'messenger.method', arg });
+  async addArea({ name }) {
+    console.log({ method: 'messenger.addArea', name });
+    return { status: 'ok' };
+  },
+
+  async changeArea({ areaId, name }) {
+    console.log({ method: 'messenger.changeArea', areaId, name });
+    return { status: 'ok' };
+  },
+
+  async deleteArea({ areaId }) {
+    console.log({ method: 'messenger.deleteArea', areaId });
+    return { status: 'ok' };
+  },
+
+  async addAccountToArea({ accountId, areaId }) {
+    console.log({ method: 'messenger.addAccountToArea', accountId, areaId });
+    return { status: 'ok' };
+  },
+
+  async deleteAccountFromArea({ accountId, areaId }) {
+    console.log({
+      method: 'messenger.deleteAccountFromArea',
+      accountId,
+      areaId
+    });
+    return { status: 'ok' };
+  },
+
+  async sendMessage({ areaId, text }) {
+    console.log({ method: 'messenger.sendMessage', areaId, text });
     return { status: 'ok' };
   },
 });
