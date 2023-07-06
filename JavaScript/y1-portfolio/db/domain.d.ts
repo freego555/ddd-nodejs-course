01dc939f53f8c244f6fc0ac6d222fbca37beac66
Ihor Rohovets <freego555@gmail.com>
2023-07-06 21:04:42 +0300

    Add types for portfolio domain

--- JavaScript/y1-portfolio/db/domain.d.ts
+++ JavaScript/y1-portfolio/db/domain.d.ts
@@ -31,3 +31,46 @@ interface Session {
   data: string;
   sessionId?: string;
 }
+
+interface Currency {
+  name: string;
+  currencyId?: string;
+}
+
+interface Exchange {
+  name: string;
+  exchangeId?: string;
+}
+
+interface Wallet {
+  exchangeId: string;
+  name: string;
+  walletId?: string;
+}
+
+interface Asset {
+  currencyId: string;
+  name: string;
+  symbol: string;
+  assetId?: string;
+}
+
+interface Rate {
+  timestamp: string;
+  baseAssetId: string;
+  quoteAssetId: string;
+  unit: number;
+  rate: number;
+  rateId?: string;
+}
+
+interface Operation {
+  timestamp: string;
+  type: 'buy' | 'sell';
+  walletId: string;
+  baseAssetId: string;
+  quoteAssetId: string;
+  quantity: number;
+  sum: number;
+  operationId?: string;
+}
